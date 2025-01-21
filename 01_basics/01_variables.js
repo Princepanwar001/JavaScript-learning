366aea7212f0e40ebfc0287c2b8f685752a4cd42
Prince Panwar <princepanwar2005@gmail.com>
2025-01-21 04:09:01 +0000

    commiting changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const  account_id = 131402
+let account_email = "alphatest@gmail.com"
+var account_password = "12345"  // never use var as no scope 
+let account_state
+account_city = "sonepat"
+console.log(account_id);
+console.table([account_id,account_email,account_password,account_state]);

42d51e90aa24c36b2c3960f704038cc10182e962
Prince Panwar <princepanwar2005@gmail.com>
2025-01-21 04:09:24 +0000

    commiting changes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,19 @@
+//alert("YOU ARE HACKED");
+//can not use in node
+
+let name = "prince"
+let age = 19
+let isloggedin = false
+
+//number==> 2,3,3
+//string==> ""
+//boolean==>true or false 
+//null==>empty or no value ,undefined & standalone value
+
+//object
+
+console.log(typeof age);
+console.log(typeof null);//it is the type of object
+
+
+
